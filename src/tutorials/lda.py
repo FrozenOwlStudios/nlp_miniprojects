0fda538b7b690b87a27827f3a31cf8921839c37a
Frozen Owl <franz.blenter@gmail.com>
2026-07-03 13:29:08 +0200

    Added lda

--- src/tutorials/lda.py
+++ src/tutorials/lda.py
@@ -10,6 +10,7 @@ This is example based on tutorial for Latent Dirichlet Anaysis
 # ======================================================================================
 from __future__ import annotations
 
+import argparse
 import sys
 from collections import Counter
 from typing import Dict, List, Optional, Set
@@ -89,7 +90,7 @@ def prepare_dictionary(tokens: pd.Series) -> gensim.corpora.Dictionary:
         count += 1
         if count > 10:
             break
-    dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+    # dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
     return dictionary
 
 
@@ -97,15 +98,28 @@ def prepare_dictionary(tokens: pd.Series) -> gensim.corpora.Dictionary:
 #                                       MAIN
 # ======================================================================================
 def main() -> None:
-    if len(sys.argv) != 2:
-        print("Correct usage: lda.py PATH_TO_CSV")
-        sys.exit()
-    documents = read_documents(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_path", help="Path to csv file")
+    parser.add_argument(
+        "-r", "--rows", type=int, default=0, help="Maximum number of rows"
+    )
+    args = parser.parse_args()
+    documents = read_documents(args.data_path)
+    if args.rows > 0:
+        documents = documents[: args.rows]
     extractor = MeaningfulWordExtractor()
-    documents = documents[:50]
     documents["tokens"] = documents["text"].apply(extractor)
     print(documents.head())
     dictionary = prepare_dictionary(documents["tokens"])
+    print(dictionary)
+    bow_corpus = [dictionary.doc2bow(doc) for doc in documents["tokens"]]
+    bow_doc_4 = bow_corpus[4]
+    for i in range(len(bow_doc_4)):
+        print(
+            'Word {} ("{}") appears {} time.'.format(
+                bow_doc_4[i][0], dictionary[bow_doc_4[i][0]], bow_doc_4[i][1]
+            )
+        )
 
 
 if __name__ == "__main__":
